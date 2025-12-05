7f7dd908c3b156c8bf61b8758213380cbaccb805
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-12-05 14:23:13 +0600

    use grouping for tracking data from parcel database by using aggregate functionality

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function run(){
     try{
        await client.connect();
       //  await client.db('admin').command({ping:1})  
-       console.log("connect to mongodb");
+      //  console.log("connect to mongodb");
 
        const database=client.db('Parcel-delivery-project');
        const dataCollection=database.collection('userData');
